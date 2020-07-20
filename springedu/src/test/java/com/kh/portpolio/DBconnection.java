fd2cbd85a5b8df37f41b081e23f590b086a9b5c1
JOJEONGMIN <mnj190>
2020-07-20 17:17:14 +0900

    0720 db 연동 완성

--- springedu/src/test/java/com/kh/portpolio/DBconnection.java
+++ springedu/src/test/java/com/kh/portpolio/DBconnection.java
@@ -1,6 +1,7 @@
 package com.kh.portpolio;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import javax.inject.Inject;
 import javax.sql.DataSource;
@@ -8,7 +9,6 @@ import javax.sql.DataSource;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -16,30 +16,43 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/root-context.xml" })
+@ExtendWith(SpringExtension.class) //스프링 컨텍스트와 junit통합
+@ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
 public class DBconnection {
-	private static final Logger logger = LoggerFactory.getLogger(DBconnection.class);
-
+	private static final Logger logger =
+			LoggerFactory.getLogger(DBconnection.class);
+	
 	@Inject
 	DataSource dataSource;
-
+	
 	@Inject
 	JdbcTemplate jdbcTemplate;
-
+	
 	@Test
 	void testdb() {
-		logger.info("DataSource : " + dataSource);
-		logger.info("JdbcTemplate : " + jdbcTemplate);
-
+		logger.info("DataSource		:" + dataSource);
+		logger.info("JdbcTemplate :" + jdbcTemplate);
+		
 		try {
 			Connection con = dataSource.getConnection();
 			Assertions.assertNotNull(con);
-			logger.info("DB 연결 성공");
-		} catch (Exception e) {
-			logger.info("DB 연결 실패");
+			logger.info("DB연결 성공!!");
+		} catch (SQLException e) {
+			logger.info("DB연결 실패!");
 			e.printStackTrace();
 		}
 	}
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
